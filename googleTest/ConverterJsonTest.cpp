a68eae3bd3f941cf883db2c0a40562bffd1843b3
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-09-04 11:35:04 +0300

    readd libs to github

---
+++ googleTest/ConverterJsonTest.cpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+#include "../include/fs_exception.h"
+#include "../include/ConverterJSON.h"
+
+bool stringEqual(const std::string& str1, const std::string& str2) {
+    if (str1.length() != str2.length()) {
+        return false;
+    }
+    for (int i = 0; i < str1.length(); i++) {
+        if (str1[i] != str2[i]) return false;
+    }
+    return true;
+}
+
+TEST(converterJSON, GetTextDocuments) {
+    auto converter = new ConverterJSON();
+    const std::string test = "C++\nwas\ntest1";
+    auto list = converter->GetTextDocuments();
+    EXPECT_GT(list.size(), 0);
+    EXPECT_TRUE(stringEqual(list[2], test));
+}
+
+TEST(converterJSON, GetResponsesLimit) {
+    auto converter = new ConverterJSON();
+    EXPECT_EQ(converter->GetResponsesLimit(), 5);
+}
+
+TEST(converterJSON, GetRequests) {
+    auto converter = new ConverterJSON();
+    auto list = converter->GetRequests();
+    EXPECT_TRUE(stringEqual(list[2], "test1"));
+}
